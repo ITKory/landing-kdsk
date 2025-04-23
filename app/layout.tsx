7a1c8cd163dd2e2086aebdaac63319142282e62c
Alex Koryakin <Skoryakin3@gmail.com>
2025-04-23 21:05:14 +0300

    master: add yandex metrics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,53 +1,80 @@
 'use client'
 import './css/style.css'
 import { Inter, Architects_Daughter } from 'next/font/google'
-import Head from 'next/head';
-import * as React from 'react';
-import type { Metadata } from 'next';
+import Script from 'next/script'
+import { useEffect } from 'react'
 
 const inter = Inter({
   subsets: ['latin'],
   variable: '--font-inter',
   display: 'swap'
 })
- 
+
 const architects_daughter = Architects_Daughter({
   subsets: ['latin'],
   variable: '--font-architects-daughter',
   weight: '400',
   display: 'swap'
 })
-const metadata: Metadata = {
-title: 'Example component',
-description: 'Learning Next.js SEO',
-};
 
 export default function RootLayout({
-  children,
-}: {
+                                     children,
+                                   }: {
   children: React.ReactNode
 }) {
- 
+  useEffect(() => {
+    // @ts-ignore
+    window.ym = window.ym || function(){(window.ym.a = window.ym.a||[]).push(arguments)}
+    // @ts-ignore
+    window.ym.l = 1*new Date()
+    // @ts-ignore
+    ym(101229823, "init", {
+      clickmap: true,
+      trackLinks: true,
+      accurateTrackBounce: true,
+      webvisor: true
+    })
+  }, [])
+
   return (
-    <html lang="en">
-      <Head>
+      <html lang="en">
+      <head>
         <title>KDSK</title>
         <meta name="description" content="Checkout our cool page" key="desc" />
         <meta property="og:title" content="Social Title for Cool Page" />
         <meta
-          property="og:description"
-          content="And a social description for our cool page"
+            property="og:description"
+            content="And a social description for our cool page"
         />
         <meta
-          property="og:image"
-          content="https://cdn.culture.ru/images/f67637ef-7daf-509e-b846-11bbe94d89bf"
+            property="og:image"
+            content="https://cdn.culture.ru/images/f67637ef-7daf-509e-b846-11bbe94d89bf"
         />
-      </Head>
+
+        <Script
+            strategy="afterInteractive"
+            src="https://mc.yandex.ru/metrika/tag.js"
+        />
+
+        <noscript>
+          <div>
+            <img
+                src="https://mc.yandex.ru/watch/101229823"
+                style={{
+                  position: 'absolute',
+                  left: '-9999px'
+                }}
+                alt=""
+            />
+          </div>
+        </noscript>
+      </head>
+
       <body className={`${inter.variable} ${architects_daughter.variable} font-inter antialiased bg-gray-900 text-gray-200 tracking-tight`}>
-        <div className="flex flex-col min-h-screen overflow-hidden">
+      <div className="flex flex-col min-h-screen overflow-hidden">
         {children}
-        </div>
+      </div>
       </body>
-    </html>
+      </html>
   )
 }
